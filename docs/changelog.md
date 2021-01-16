b1602b169ba6bcf817a92e3734f93150a4eb20e9
Artem Malyshev <proofit404@gmail.com>
2021-01-16 15:42:29 +0300

    refactor: initialize repository #3

---
+++ docs/changelog.md
@@ -0,0 +1,2 @@
+<p align="center">&mdash; ⭐️ &mdash;</p>
+<p align="center"><i>The `stories-sentry` library is part of the SOLID python family.</i></p>
---
+++ src/stories_sentry/exceptions.py
@@ -0,0 +1,5 @@
+"""A set of stories-sentry' exceptions."""
+from _stories_sentry.exceptions import StorySentryError
+
+
+__all__ = ["StorySentryError"]
